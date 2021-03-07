de083cab3c87b7a340fa0f14841aa1ab0bf5ebf8
Pavlo Ilin <ilin.pa@gmail.com>
2021-03-07 13:41:46 +0100

    Add compound assignment operators to tvec2

--- src/math.hpp
+++ src/math.hpp
@@ -82,30 +82,39 @@ struct tvec2 {
 		y /= s;
 		return *this;
 	}
+
+	tvec2& operator+=(tvec2 const& v) {
+		x += v.x;
+		y += v.y;
+		return *this;
+	}
+	tvec2& operator-=(tvec2 const& v) {
+		x -= v.x;
+		y -= v.y;
+		return *this;
+	}
+	tvec2& operator*=(tvec2 const& v) {
+		x *= v.x;
+		y *= v.y;
+		return *this;
+	}
+	tvec2& operator/=(tvec2 const& v) {
+		x /= v.x;
+		y /= v.y;
+		return *this;
+	}
 	
-	friend tvec2 operator+(tvec2 const& a, tvec2 const& b) {
-		tvec2 res(a);
-		res.x += b.x;
-		res.y += b.y;
-		return res;
+	friend tvec2 operator+(tvec2 a, tvec2 const& b) {
+		return a += b;
 	}
-	friend tvec2 operator-(tvec2 const& a, tvec2 const& b) {
-		tvec2 res(a);
-		res.x -= b.x;
-		res.y -= b.y;
-		return res;
+	friend tvec2 operator-(tvec2 a, tvec2 const& b) {
+		return a -= b;
 	}
-	friend tvec2 operator*(tvec2 const& a, tvec2 const& b) {
-		tvec2 res(a);
-		res.x *= b.x;
-		res.y *= b.y;
-		return res;
+	friend tvec2 operator*(tvec2 a, tvec2 const& b) {
+		return a *= b;
 	}
-	friend tvec2 operator/(tvec2 const& a, tvec2 const& b) {
-		tvec2 res(a);
-		res.x /= b.x;
-		res.y /= b.y;
-		return res;
+	friend tvec2 operator/(tvec2 a, tvec2 const& b) {
+		return a /= b;
 	}
 	
 };
