f1c4bf65cf6c5c91a6d6bf4a4ed68618e85e6dc6
Pavlo Ilin <ilin.pa@gmail.com>
2021-12-24 23:12:35 +0100

    Disable color adjustment in simple pixel shader

--- hlsl/simple.ps.hlsl
+++ hlsl/simple.ps.hlsl
@@ -5,7 +5,7 @@ float4 main(sPSSimple pin) : SV_TARGET
 
 	float4 clr = float4(1.0f, 1.0f, 1.0f, 1.0f);
 	float3 rgb = pin.clr.rgb;
-	rgb = rgb * 0.55 + 0.225;
+	//rgb = rgb * 0.55 + 0.225;
 	clr.rgb = rgb;
 	clr.a = pin.clr.a;
 	return clr;
